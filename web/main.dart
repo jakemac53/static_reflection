96fea698f4847a5df07426f6143c46983cac3f73
Jacob MacDonald <jakemac@google.com>
2020-09-03 12:08:06 -0700

    organize code better

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,37 @@ import 'package:static_reflection/reflection.dart';
 
 import 'user.dart';
 
+// Only used in the "bad" example below to demonstrate the impact of a map like
+// this on tree shaking.
+var mirrors = {
+  Login: loginMirror,
+  Foo: fooMirror,
+  User: userMirror,
+};
+
+main() {
+  print(jsonDecode<Login>(
+    {
+      'user': {'name': 'Joe', 'age': 1, 'id': '1234'},
+      'password': '@dm1n'
+    },
+    // Un-comment this (and comment out next line) to see the bad behavior.
+    // mirrors[Login] as ClassMirror<Login>)
+    loginMirror,
+  ));
+}
+
+// Note that this will get tree shaken including the entire class. We can have
+// unused references to these mirrors safely.
+void makeFoo() {
+  jsonDecode<Foo>({'zap': 1}, fooMirror);
+}
+
+// Below here would all be code generated based on something like:
+//
+// external ClassMirror<Login> loginMirror;
+// external ClassMirror<Login> fooMirror;
+
 User getUser(Login login) => login.user;
 void setUser(Login login, User user) => login.user = user;
 String getPassword(Login login) => login.password;
@@ -157,21 +188,3 @@ const fooMirror =
     metadata: [fromJson],
   ),
 });
-
-var mirrors = {
-  Login: loginMirror,
-  Foo: fooMirror,
-  User: userMirror,
-};
-
-main() {
-  print(jsonDecode<Login>(
-    {
-      'user': {'name': 'Joe', 'age': 1, 'id': '1234'},
-      'password': '@dm1n'
-    },
-    // Comment out this to get the bad behavior.
-    // mirrors[Login] as ClassMirror<Login>)
-    loginMirror,
-  ));
-}
